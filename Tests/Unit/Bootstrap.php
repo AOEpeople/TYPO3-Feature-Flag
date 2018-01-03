311a4ea367b5e55853238e092e7a0ddae65e0802
Chetan Thapliyal <chetan.thapliyal@aoe.com>
2018-01-03 07:55:40 +0100

    [TASK] Add Travis job configuration
    - Include Travis job configuration.
    - Update composer and PHPUnit unit and functional test configuration
      in order to run the tests on Travis.
    - Include scrutinizer configuration for code coverage data.

--- Tests/Unit/Bootstrap.php
+++ Tests/Unit/Bootstrap.php
@@ -1,5 +1,18 @@
 <?php
-require_once dirname(__FILE__) . '/../../../../../typo3/sysext/core/Build/UnitTestsBootstrap.php';
+
+$typo3DocumentRoot = getenv('TYPO3_PATH_WEB');
+
+if ($typo3DocumentRoot === false) {
+    die(
+        'Error: TYPO3 document root not defined. ' . "\n"
+            . 'Please set "TYPO3_PATH_WEB" environment variable to TYPO3 root directory '
+            . 'and try again.' . "\n"
+    );
+} else {
+    $typo3DocumentRoot = rtrim($typo3DocumentRoot, '/') . '/';
+}
+
+require_once $typo3DocumentRoot . 'typo3/sysext/core/Build/UnitTestsBootstrap.php';
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase_object'] = array(
     'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\NullBackend',
