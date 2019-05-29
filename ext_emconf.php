ef0af9607bdd9e1efd4a5e759fb0482f7e7a51a1
Christian Krzeminski <christian.krzeminski@aoe.com>
2019-05-29 09:01:14 +0200

    [FEATURE] Update composer dependencies for TYPO3V8

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '5.0.0',
+    'version' => '5.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-7.6.99'
+            'typo3' => '7.6.0-8.7.99'
         ],
         'conflicts' => [],
         'suggests' => [],
