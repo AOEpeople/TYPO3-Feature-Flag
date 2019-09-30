0adca470a5dc5b070d104f0f9ac6d0baeb71d393
Thomas Schenk <Thomas.Schenk@aoe.com>
2019-09-30 08:50:10 +0200

    [TASK] removes TYPO3 V7 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.1.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99'
+            'typo3' => '8.7.0-8.7.99'
         ],
         'conflicts' => [],
         'suggests' => [],
