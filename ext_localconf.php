41f1810b98e2b5efef94c123cdbde8424c2cdc7f
thomas.layh <thomas.layh@aoe.com>
2022-09-22 12:15:31 +0200

    [TASK] delete obsolete task and switch to symfony command

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,5 @@
 <?php
+
 use Aoe\FeatureFlag\Form\Element\FeatureFlagBehaviourFormSelectElement;
 use Aoe\FeatureFlag\Form\Element\FeatureFlagFormSelectElement;
 use Aoe\FeatureFlag\Form\Element\InfoTextElement;
@@ -22,9 +23,4 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1634736513] = [
     'nodeName' => 'infoText',
     'priority' => '70',
     'class' => InfoTextElement::class,
-];
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Aoe\FeatureFlag\System\Typo3\Task\FlagEntriesTask::class] = [
-    'extension' => 'feature_flag',
-    'title' => 'LLL:EXT:feature_flag/Resources/Private/Language/locallang.xlf:feature_flag.scheduler_task.flag_entries.title',
-    'description' => 'LLL:EXT:feature_flag/Resources/Private/Language/locallang.xlf:feature_flag.scheduler_task.flag_entries.description',
 ];
